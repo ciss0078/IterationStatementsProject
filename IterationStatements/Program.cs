3030db3dfbf26872e20e7c181a53b77c0bf3c967
Abdoulahi Cisse <sentel.nv@gmail.com>
2020-10-15 18:02:36 -0700

    iteration work done

--- IterationStatements/Program.cs
+++ IterationStatements/Program.cs
@@ -11,33 +11,44 @@ namespace IterationStatements
 
             //Create a List called "numbers" - DONE
             var numbers = new List<int>();
-            
+
             //Create a variable of type int and initialize the variable with a value of 0
 
-
+            int i = 0;
 
             // Create a do-while loop
+            do
             {
                 // Increment your variable by 1
-
+                i++;
                 // Then add your variable to "numbers"
-                numbers.Add(/* variable name goes here */);
-            }
+                numbers.Add( i/* variable name goes here */);
+            } while (i < 100);
             // While your variable is less than 100
 
 
 
             // Create a while loop
             // While your variable is less than 200
+            int j = 0;
+            while(j < 200)
             {
+                j++;
+                
                 // Increment your variable by 1
                 // Then add your variable to "numbers"
+                numbers.Add(j);
+                
             }
 
             Console.WriteLine("Increase:");
 
             // Create a foreach loop
             // Write your variable to the console
+            foreach(var number in numbers ) 
+            {
+                Console.WriteLine(number);
+            }
 
             Console.WriteLine("");
             Console.WriteLine("Decrease:");
@@ -47,7 +58,14 @@ namespace IterationStatements
             // in your conditional, as long as i is less than or equal to the amount of items in "numbers" (numbers.Count)
             // and as long as i is greater than or equal to 0
             // Decrement i by 1
+            for (int k = 199; k <= numbers.Count -1; k--)
             {
+                if(k >= 0)
+                {
+                    Console.WriteLine(numbers[k]);
+                }
+                
+
                 // Write to the console "numbers" at index i
             }
         }
